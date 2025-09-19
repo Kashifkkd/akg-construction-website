5fe609e1dc751102bc372a711c3b9e6a815defac
Kashif <kashifdeshmukh786@gmail.com>
2025-09-20 00:46:59 +0530

    analytics added

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -5,7 +5,7 @@ import { Award, Users, Clock, Target, ChevronLeft, ChevronRight, ArrowRight, Pla
 import { Button } from '@/components/ui/button'
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
-
+import { Analytics } from "@vercel/analytics/next" add this
 const slideshowImages = [
   {
     src: "https://images.unsplash.com/photo-1581094794329-c8112a89af12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
